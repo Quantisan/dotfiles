69ea0a378f7e657462a99bf65fb90c8433749fca
Paul Lam <paul@quantisan.com>
2023-03-22 13:56:02 +0900

    add to list of mason ensure_installed

---
+++ .config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        -- Clojure
+        "clj-kondo",
+        "clojure-lsp",
+        "joker",
+
+        -- Python
+        "flake8",
+        "black",
+        "pyright",
+
+        -- Terraform
+        "terraform-ls",
+        "tflint",
+
+        -- Javascript
+        "rome",
+
+        -- Lua
+        "lua-language-server",
+        "stylua",
+
+        -- Others
+        "json-lsp",
+        "shfmt",
+      },
+    },
+  },
+}

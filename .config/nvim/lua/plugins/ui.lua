e53f8650070e1b76465c4e1d8b90175313765105
Paul Lam <paul@quantisan.com>
2023-04-09 16:14:15 +0900

    set some lazyvim ui options

---
+++ .config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 1000,
+    },
+  },
+}

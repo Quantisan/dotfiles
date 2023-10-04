35b1b638455efb59cc07da5df7a7dc5073efc1c8
Paul Lam <paul@quantisan.com>
2023-10-04 11:25:55 +0900

    enable none-ls with Python formatters

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -13,6 +13,7 @@ return {
         "black",
         "isort",
         "ruff",
+        "mypy",
         "pyright",
 
         -- Terraform
@@ -31,5 +32,21 @@ return {
         "shfmt",
       },
     },
+  },
+
+  {
+    "nvimtools/none-ls.nvim",
+    opts = function()
+      local nls = require("null-ls")
+      return {
+        root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
+        sources = {
+          nls.builtins.diagnostics.mypy,
+          nls.builtins.formatting.isort,
+          nls.builtins.formatting.black,
+          nls.builtins.formatting.ruff,
+        },
+      }
+    end,
   },
 }

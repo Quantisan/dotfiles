81d3d6c4ab2e268e6394752681232e32f56f9cfd
Paul Lam <paul@quantisan.com>
2023-11-03 09:23:27 +0900

    remove none-ls as lazyvim migrated away from it

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -42,23 +42,7 @@ return {
           mason = false,
           autostart = false,
         },
-        pylsp = {}
       },
     },
-  },
-
-  {
-    "nvimtools/none-ls.nvim",
-    opts = function()
-      local nls = require("null-ls")
-      return {
-        root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
-        sources = {
-          nls.builtins.diagnostics.mypy,
-          nls.builtins.formatting.isort,
-          nls.builtins.formatting.black,
-        },
-      }
-    end,
   },
 }

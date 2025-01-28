60065db35be47221dc4af6e375a159ccd3954453
Paul Lam <paul@quantisan.com>
2025-01-29 06:26:10 +0900

    add leader key and action PaneSelect

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,5 +1,6 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
+local act = wezterm.action
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -8,6 +9,12 @@ local config = wezterm.config_builder()
 
 config.color_scheme = "Tokyo Night"
 config.font_size = 13.0
+
+config.leader = { key = "Space", mods = "CTRL" }
+
+config.keys = {
+    { key = "p", mods = "CTRL", action = act.PaneSelect },
+}
 
 -- and finally, return the configuration to wezterm
 return config

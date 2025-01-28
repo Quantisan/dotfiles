de219a97943613b1cd52da4ecca6822b25e5a833
Paul Lam <paul@quantisan.com>
2025-01-28 17:23:44 +0900

    set color_schema

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,8 +6,8 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
+config.color_scheme = "Tokyo Night"
 config.font_size = 13.0
 
 -- and finally, return the configuration to wezterm
 return config
-

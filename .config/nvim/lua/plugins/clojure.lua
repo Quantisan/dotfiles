f4e51748f1e6ee9a8c960a37d05f56aac35c7590
Paul Lam <paul@quantisan.com>
2025-02-03 08:48:03 +0900

    feat: Add vim-easy-align plugin for Clojure editing
    feat: Configure vim-easy-align with `ga` keymaps in Clojure plugin

--- .config/nvim/lua/plugins/clojure.lua
+++ .config/nvim/lua/plugins/clojure.lua
@@ -1,3 +1,9 @@
 return {
   { "gpanders/nvim-parinfer" },
+  {
+    "junegunn/vim-easy-align",
+    keys = {
+      { "ga", "<Plug>(EasyAlign)", mode = { "n", "x" }, desc = "Easy Align" },
+    },
+  },
 }

2c58e3d141af0d0be66862735c76890e160079f1
Paul Lam <paul@quantisan.com>
2025-03-03 06:25:02 +0900

    set nvim neotree opens to the right

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -2,6 +2,10 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
+      window = {
+        position = "right",
+      },
+
       filesystem = {
         filtered_items = { hide_dotfiles = false },
       },

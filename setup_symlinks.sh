72d1ab66487cfbb7fd0d3226f85931566358898c
Paul Lam <paul@quantisan.com>
2025-08-23 05:29:27 +0900

    symlink MacWhisper and Spotify caches

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -16,10 +16,16 @@ ln -shf "$NUB/Projects" "$HOME/Projects"
 # Ensure parent exists and symlink Homebrew cache into Library/Caches
 mkdir -p "$HOME/Library/Caches"
 ln -shf "$NUB/Library/Caches/Homebrew" "$HOME/Library/Caches/Homebrew"
+ln -shf "$NUB/Library/Caches/com.goodsnooze.MacWhisper" "$HOME/Library/Caches/com.goodsnooze.MacWhisper"
+ln -shf "$NUB/Library/Caches/com.spotify.client" "$HOME/Library/Caches/com.spotify.client"
 
 mkdir -p "$HOME/Library/Homebrew"
 ln -shf "$NUB/Library/Homebrew/downloads/" "$HOME/Library/Homebrew/downloads"
 ln -shf "$NUB/Library/Homebrew/Casks/" "$HOME/Library/Homebrew/Casks"
+
+# Application Support
+mkdir -p "$HOME/Library/Application\ Support"
+ln -shf "$NUB/Library/Application\ Support/MacWhisper/" "$HOME/Library/Application\ Support/MacWhisper"
 
 # XDG-like data/cache locations
 mkdir -p "$HOME/.local"
